2ba6123d8579ae1aa7ae982d0582558344544d95
Icaro Temponi <icarohs7@gmail.com>
2024-11-09 13:50:14 -0300

    Add NInputSearchBar widget
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

---
+++ lib/src/search_resources/n_input_search_bar.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import 'n_basic_search_delegate.dart';
+import 'n_search_suggestion.dart';
+
+class NInputSearchBar extends StatelessWidget {
+  final Iterable<NSearchSuggestion> suggestions;
+  final String? searchHint;
+  final ShapeBorder? shape;
+  final double? elevation;
+  final Widget? icon;
+
+  const NInputSearchBar({
+    required this.suggestions,
+    this.searchHint,
+    this.shape,
+    this.elevation,
+    this.icon,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 24),
+      child: Material(
+        elevation: elevation ?? 6,
+        shape: shape ?? StadiumBorder(),
+        child: InkWell(
+          onTap: () => showSearch(
+            context: context,
+            delegate: NBasicSearchDelegate(suggestions),
+          ),
+          customBorder: shape ?? StadiumBorder(),
+          child: Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                child: icon ?? Icon(Icons.search),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+                  child: Text(
+                    searchHint ?? 'Pesquisar',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/src/search_resources/search_resources.dart
+++ lib/src/search_resources/search_resources.dart
@@ -1,2 +1,3 @@
 export 'n_basic_search_delegate.dart';
+export 'n_input_search_bar.dart';
 export 'n_search_suggestion.dart';
