0bb1c6fb07b7521daad3109e16b6558766c63618
Icaro Temponi <icarohs7@gmail.com>
2024-12-12 10:06:23 -0300

    Replace store_redirect by launch_app_store
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- lib/src/adapters/wrappers.dart
+++ lib/src/adapters/wrappers.dart
@@ -1,10 +1,10 @@
 import 'package:asuka/asuka.dart';
 import 'package:flutter/material.dart';
-import 'package:store_redirect/store_redirect.dart' as store_redirect;
+import 'package:launch_app_store/launch_app_store.dart' as store_redirect;
 import 'package:url_launcher/url_launcher.dart' as url_launcher;
 
 Future<void> redirectToStore({String? androidAppId, String? iosAppId}) =>
-    store_redirect.StoreRedirect.redirect(androidAppId: androidAppId, iOSAppId: iosAppId);
+    store_redirect.LaunchReview.launch(androidAppId: androidAppId, iOSAppId: iosAppId);
 
 Future<bool> launchUrl(Uri url, {bool openOnWebView = false}) => url_launcher.launchUrl(url,
     mode: openOnWebView
