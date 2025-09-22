6fee283e094478c1523cdd1cd8c66cee154406ba
Icaro Temponi <icarohs7@gmail.com>
2025-09-22 19:59:08 -0300

    Add wrapper to share
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- lib/src/adapters/wrappers.dart
+++ lib/src/adapters/wrappers.dart
@@ -1,5 +1,6 @@
 import 'package:asuka/asuka.dart';
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart' as share_plus;
 import 'package:store_redirect/store_redirect.dart' as store_redirect;
 import 'package:url_launcher/url_launcher.dart' as url_launcher;
 
@@ -14,3 +15,10 @@ Future<bool> launchUrl(Uri url, {bool openOnWebView = false}) => url_launcher.la
 ScaffoldFeatureController<SnackBar, SnackBarClosedReason> showSnackbar(SnackBar snackbar) {
   return Asuka.showSnackBar(snackbar);
 }
+
+Future<ShareResult> share(ShareParams params) {
+  return share_plus.SharePlus.instance.share(params);
+}
+
+typedef ShareParams = share_plus.ShareParams;
+typedef ShareResult = share_plus.ShareResult;
