68a73367d197f762faeef6891d34fa10691ea59e
Icaro Temponi <icarohs7@gmail.com>
2025-12-08 13:42:24 -0300

    Use dart as the scripting language instead of python
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

---
+++ build.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: avoid_print
+
+import 'dart:io';
+
+Future<void> runCommand(String command) async {
+  print('Running: $command\n');
+
+  final result = await Process.run(command, [], runInShell: true);
+
+  if (result.stdout.toString().isNotEmpty) stdout.write(result.stdout);
+  if (result.stderr.toString().isNotEmpty) stderr.write(result.stderr);
+
+  if (result.exitCode != 0) {
+    print('Command failed: $command');
+    print('Exit code: ${result.exitCode}');
+    exit(result.exitCode);
+  }
+}
+
+void main() async {
+  final commands = [
+    'flutter packages get',
+    'flutter pub run build_runner build',
+    'flutter analyze',
+    'flutter test --coverage',
+  ];
+
+  for (final command in commands) {
+    await runCommand(command);
+  }
+
+  print('\nModule succeeded building');
+}
