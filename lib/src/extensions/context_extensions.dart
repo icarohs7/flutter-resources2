4f9f10c5564431557376473f38a878c219ea721c
Icaro Temponi <icarohs7@gmail.com>
2026-04-06 18:18:08 -0300

    Add `isSliverCollapsed` extension to `BuildContext`
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- lib/src/extensions/context_extensions.dart
+++ lib/src/extensions/context_extensions.dart
@@ -5,4 +5,11 @@ extension NContextExtensions on BuildContext {
   ColorScheme get colorScheme => theme.colorScheme;
 
   Color get secondaryColor => colorScheme.secondary;
+
+  /// Checks if the sliver is collapsed by comparing the current extent to the min extent.
+  bool get isSliverCollapsed {
+    final settings = dependOnInheritedWidgetOfExactType<FlexibleSpaceBarSettings>();
+    if (settings == null) return false;
+    return settings.currentExtent <= settings.minExtent + 1;
+  }
 }
