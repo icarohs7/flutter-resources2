e84412df6ad3fa94788dda3adfb93c3de801c39e
Icaro Temponi <icarohs7@gmail.com>
2026-05-17 07:39:27 -0300

    feat: export GestureConfig from flutter_resources2
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

--- lib/flutter_resources2.dart
+++ lib/flutter_resources2.dart
@@ -19,7 +19,8 @@ export 'package:extended_image/extended_image.dart'
         ExtendedFileImageProvider,
         ExtendedNetworkImageProvider,
         ExtendedAssetImageProvider,
-        ExtendedMemoryImageProvider;
+        ExtendedMemoryImageProvider,
+        GestureConfig;
 export 'package:flutter_image_compress/flutter_image_compress.dart' show XFile;
 export 'package:flutter_secure_storage/flutter_secure_storage.dart';
 export 'package:hive_ce_flutter/hive_flutter.dart';
